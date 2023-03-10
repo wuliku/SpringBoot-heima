a2a47e013febf64d49b82a257cad0b484aa588ca
mcl <mcl@163.com>
2023-03-10 22:40:17 +0800

    测试

--- springboot_16_redis/src/main/java/com/itheima/Springboot16RedisApplication.java
+++ springboot_16_redis/src/main/java/com/itheima/Springboot16RedisApplication.java
@@ -8,6 +8,26 @@ public class Springboot16RedisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Springboot16RedisApplication.class, args);
+        Object obj = null;
+        String s = "abc=bdc=111";
+        String[] split = s.split("=");
+        String replace = "111"; //目标key
+        String targetValue = "222"; //目标value
+        String replace1 = "";
+        for (int i = 0; i < split.length; i++) {
+            if (split[i].endsWith(replace) || split[i].equals(replace)) {
+                //判断下一个元素是否存在
+                //可以看下是可以通过那个分割
+//                split[i] = targetValue;
+                s = s.replace(split[i], targetValue);
+            }
+            //替换
+
+        }
+        //重新将string[]转换成string
+        System.out.println(replace1);
     }
+
+
 
 }
